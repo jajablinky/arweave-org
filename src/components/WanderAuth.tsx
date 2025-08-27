0e19b250bc80afdd6bc588272a66144352c83291
Geoffrey Millar <gmillar6@gmail.com>
2025-08-27 13:15:01 -0700

    fix: enable button in WanderAuth component for improved user interaction
    - Changed the button property from false to true in the WanderAuth component to enhance user engagement during authentication.

--- src/components/WanderAuth.tsx
+++ src/components/WanderAuth.tsx
@@ -9,7 +9,7 @@ export default function WanderAuth() {
       clientId: "FREE_TRIAL",
       ui: { launcher: false },
       showLauncher: false,
-      button: false,
+      button: true,
     });
     wanderRef.current = wander;
     try {
