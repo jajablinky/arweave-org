bced1624d31cee366528f7ec9803ba28876fedd6
Geoffrey Millar <gmillar6@gmail.com>
2025-08-27 14:08:21 -0700

    fix: disable button in WanderAuth

--- src/components/WanderAuth.tsx
+++ src/components/WanderAuth.tsx
@@ -9,7 +9,7 @@ export default function WanderAuth() {
       clientId: "FREE_TRIAL",
       ui: { launcher: false },
       showLauncher: false,
-      button: true,
+      button: false,
     });
     wanderRef.current = wander;
     try {
